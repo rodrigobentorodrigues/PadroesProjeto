e32be861105338a33205a1ad0e9cde4924ec127d
natansevero <natanseverolp@gmail.com>
2017-10-07 20:05:31 -0300

    Problema com InputStream resolvido

--- src/main/java/com/ifpb/padroes/commands/ImportarFeriados.java
+++ src/main/java/com/ifpb/padroes/commands/ImportarFeriados.java
@@ -5,6 +5,7 @@
  */
 package com.ifpb.padroes.commands;
 
+import com.ifpb.padroes.entidades.Feriado;
 import com.ifpb.padroes.interfaces.Command;
 import com.ifpb.padroes.interfaces.FeriadoDao;
 import java.io.File;
@@ -12,8 +13,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.inject.Inject;
@@ -21,6 +27,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 /**
  *
@@ -37,6 +45,7 @@ public class ImportarFeriados implements Command {
         Part path = null;
         OutputStream outputStream = null;
         File file = null;
+        InputStream inputStream = null;
         try {
             path = requisicao.getPart("csv");
             System.out.println(path);
@@ -52,21 +61,22 @@ public class ImportarFeriados implements Command {
             file = new File(upPath + File.separator + "arq.csv");
             outputStream = new FileOutputStream(file);
             
+            // Pegando o inputStream do path para depois fecha-lo no finally
+            inputStream = path.getInputStream();
+            
             // Pego os dados do CSV importado e ponho dentro de arq.csv
             int read = 0;
             byte[] bytes = new byte[1024];
-            while((read = path.getInputStream().read(bytes)) != -1) outputStream.write(bytes, 0, read);
-            
-            this.tratarCSV(file);
+            while((read = inputStream.read(bytes)) != -1) outputStream.write(bytes, 0, read);
             
         } catch (IOException | ServletException ex) {
             Logger.getLogger(ImportarFeriados.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-//            try {
-//                if(path.getInputStream() != null) path.getInputStream().close();
-//            } catch (IOException ex) {
-//                Logger.getLogger(ImportarFeriados.class.getName()).log(Level.SEVERE, null, ex);
-//            }
+            try {
+                if(inputStream != null) path.getInputStream().close();
+            } catch (IOException ex) {
+                Logger.getLogger(ImportarFeriados.class.getName()).log(Level.SEVERE, null, ex);
+            }
             if (outputStream != null) {
 		try {		
                     outputStream.close();
@@ -74,6 +84,13 @@ public class ImportarFeriados implements Command {
                     e.printStackTrace();
                 }
             }
+        }
+        
+        // Chamando método para tratar o csv
+        try {
+            if(file != null) this.tratarCSV(file);
+        } catch (IOException ex) {
+            Logger.getLogger(ImportarFeriados.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         try {
@@ -86,17 +103,17 @@ public class ImportarFeriados implements Command {
     private void tratarCSV(File file) throws FileNotFoundException, IOException {
         System.out.println(file);
         Reader reader = new FileReader(file);
-//        Iterable<CSVRecord> registros = CSVFormat.RFC4180.parse(reader);
-//        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/mm/yyyy");
-//        List<Feriado> feriados = new ArrayList<>();
+        Iterable<CSVRecord> registros = CSVFormat.RFC4180.parse(reader);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/mm/yyyy");
+        List<Feriado> feriados = new ArrayList<>();
 //        
-//        for(CSVRecord registro: registros) {
-//            LocalDate localDate = LocalDate.parse(registro.get(0), dateTimeFormatter);
-//            String nomeFeriado = registro.get(2);
-//            Feriado f = new Feriado(nomeFeriado, localDate);
-//            System.out.println(f.toString());
-////            feriados.add(f);
-//        }
+        for(CSVRecord registro: registros) {
+            LocalDate localDate = LocalDate.parse(registro.get(0), dateTimeFormatter);
+            String nomeFeriado = registro.get(2);
+            Feriado f = new Feriado(nomeFeriado, localDate);
+            System.out.println(f.toString());
+//            feriados.add(f);
+        }
         
 //        dao.persistirTodosFeriados(feriados);
     }
