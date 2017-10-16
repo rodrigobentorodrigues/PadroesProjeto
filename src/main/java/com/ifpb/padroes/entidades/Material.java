509115479558518fa548575c9b69bb39d73362d9
rudanlucena <rudanlucena@hotmail.com>
2017-10-16 15:53:29 -0300

    add seq_num_tombamento, add novo menu

--- src/main/java/com/ifpb/padroes/entidades/Material.java
+++ src/main/java/com/ifpb/padroes/entidades/Material.java
@@ -7,12 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 
 @Entity
 public class Material implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "seq_tombamento" , sequenceName = "seq_tombamento", initialValue = 1111111, allocationSize = 2)
+    @GeneratedValue(generator = "seq_tombamento", strategy = GenerationType.SEQUENCE)
+    @Column(length = 7)
     private int tombamento;
     private String nome;
     
